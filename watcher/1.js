edfd4fecd7b6d7469c1e65a8c560d00f600287c4
Foxy-Loxy <kavramenko2k@gmail.com>
2021-10-11 14:42:49 +0300

    Removing garbage

--- watcher/1.js
+++
@@ -1,8 +0,0 @@
-async function main() {
-    var value = await Promise.resolve('Hey there');
-    console.log('inside: ' + value);
-    return value;
-}
-
-var text = main();  
-console.log('outside: ' + text)
